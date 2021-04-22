feece5c1f832f1ab7ad0e1e08a4d3116ee7f372c
johanmelkersson <melkersson.j@gmail.com>
2021-04-22 15:06:46 +0200

    parallax changes

--- SoftArch/Assets/Scripts/Parallax/CameraFollow.cs
+++ SoftArch/Assets/Scripts/Parallax/CameraFollow.cs
@@ -19,6 +19,6 @@ public class CameraFollow : MonoBehaviour
         //transform.position += new Vector3(deltaMovement.x * parallaxEffect.x, deltaMovement.y * parallaxEffect.y, 0);
         //lastCameraPos = cameraTransform.position;
 
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 13, player.transform.position.z - 25);
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 13, player.transform.position.z - 30);
     }
 }
