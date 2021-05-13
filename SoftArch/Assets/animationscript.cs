8014855997d4777da67713776e86ec9d211dedf2
tila18 <43038427+tila18@users.noreply.github.com>
2021-05-13 15:52:30 +0200

    animation
    added the animation controller and the related script

--- SoftArch/Assets/animationscript.cs
+++ SoftArch/Assets/animationscript.cs
@@ -1,6 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+
+/*
+ * By Tinea Larsson
+ * 
+ * Script to update the parameters needed in the animation controller
+ */
+
+// TO DO: 
+
+// Change FlipAnimation() to only occur if character has landed from it's previous jump
+
+// Add idleTimer function
 
 public class animationscript : MonoBehaviour
 {
@@ -8,8 +21,10 @@ public class animationscript : MonoBehaviour
     float vY;
     public float idleTimer;
     public Rigidbody rb;
-    Animator animator;
 
+    bool isFacingRight;
+
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
@@ -20,10 +35,56 @@ public class animationscript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        vX = rb.velocity.x;
-        vY = rb.velocity.y;
-        animator.SetFloat("VelocityX", vX);
-        animator.SetFloat("VelocityX", vY);
+        UpdateParameters();
+    }
 
+    private void FixedUpdate()
+    {
+        UpdateParameters();
+    }
+
+    void UpdateParameters()
+    {
+        // rotate animation to match its direction, only when switching direction
+        if ((isFacingRight && rb.velocity.x < 0 )||(!isFacingRight && rb.velocity.x > 0))
+        {
+            FlipAnimation();
+        }
+
+        // Set velocity X to always be positive, direction does not matter for selecting the correct animation
+        vX = (float)Math.Round(Mathf.Abs(rb.velocity.x) * 100f / 100f);
+
+        //Debug.Log(vX);
+
+        // if gravity is currently set to normal
+        if (rb.useGravity)
+        {
+            vY = (float)Math.Round(rb.velocity.y * 100f / 100f);
+            animator.SetBool("GravityFlipped", false);
+        }
+        else
+        {
+            vY = rb.velocity.y; //
+            animator.SetBool("GravityFlipped", true);
+        }
+
+
+        animator.SetFloat("VelocityX", vX);
+        animator.SetFloat("VelocityY", vY);
+
+        
+
+        //Debug.Log(animator.GetFloat("VelocityX"));
+    }
+
+    void FlipAnimation()
+    {
+        // switches the bool
+        isFacingRight = !isFacingRight;
+
+        // switches the local scale to an inversion of itself
+        Vector3 newScale = transform.localScale;
+        newScale.x *= -1;
+        transform.localScale = newScale;
     }
 }
